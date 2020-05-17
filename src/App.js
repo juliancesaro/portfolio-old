3b65577553260849a8d40440bc0d6c0c77abf19b
Julian Cesaro <42017431+juliancesaro@users.noreply.github.com>
2020-05-17 17:39:12 +1000

    Added particles

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Particles from 'react-particles-js';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="particleswrapper">
+      <Particles className="particles" params={{ 
+          particles: { 
+            number: { 
+              value: 200, 
+              density: { 
+                enable: true, 
+                value_area: 1000, 
+              } 
+            }, 
+            line_linked: {
+              enable: true,
+	            opacity: 0
+            }
+          }, 
+        }}  />
+        </div>
+        <div className="navbar">
+          <h1>Julian Cesaro</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
