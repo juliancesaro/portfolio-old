4979ababf1a7272d3808166759e80b8ef0c79ddb
Julian Cesaro <42017431+juliancesaro@users.noreply.github.com>
2020-05-18 03:49:37 +1000

    added active navlink

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import julian from "../images/julian.png";
 
@@ -9,15 +9,31 @@ function Navbar() {
       <img src={julian} alt="Julian" width={80}></img>
       <p className="name">Julian Cesaro</p>
       <div className="links">
-        <Link className="link" to="/">
-          About
-        </Link>
-        <Link className="link" to="/projects">
-          Projects
-        </Link>
-        <Link className="link" to="/resume">
-          Resume
-        </Link>
+        <div className="linkwrapper">
+          <NavLink exact className="link" activeClassName="activelink" to="/">
+            About
+          </NavLink>
+        </div>
+        <div className="linkwrapper">
+          <NavLink
+            exact
+            className="link"
+            activeClassName="activelink"
+            to="/projects"
+          >
+            Projects
+          </NavLink>
+        </div>
+        <div className="linkwrapper">
+          <NavLink
+            exact
+            className="link"
+            activeClassName="activelink"
+            to="/resume"
+          >
+            Resume
+          </NavLink>
+        </div>
       </div>
     </div>
   );
