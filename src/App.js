f52e86f3783b65f64e3ac4d11175dfc6ea484a91
juliancesaro <julianjcesaro@gmail.com>
2020-05-18 00:28:22 +1000

    added content

--- src/App.js
+++ src/App.js
@@ -1,83 +1,91 @@
 import React from "react"
 import "./App.css"
 import Particles from "react-particles-js"
+import julian from "./images/julian.png"
 
 function App() {
   return (
     <div className="App">
-      <div className="particleswrapper">
-        <Particles
-          className="particles"
-          params={{
-            particles: {
-              number: { value: 50, density: { enable: true, value_area: 800 } },
-              color: { value: ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"] },
-              shape: {
-                type: "circle",
-                stroke: { width: 0, color: "#000000" },
-                polygon: { nb_sides: 5 },
-                image: { src: "img/github.svg", width: 100, height: 100 },
-              },
-              opacity: {
-                value: 0.5,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false,
-                },
-              },
-              size: {
-                value: 3,
-                random: true,
-                anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-              },
-              line_linked: {
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: { value: 50, density: { enable: true, value_area: 800 } },
+            color: { value: ["#cc892b", "#1cd6b1", "#8c4ec7", "#8bc74e"] },
+            opacity: {
+              value: 0.5,
+              random: false,
+              anim: {
                 enable: true,
-                distance: 150,
-                color: "#ffffff",
-                opacity: 0.4,
-                width: 1,
-              },
-              move: {
-                enable: true,
-                speed: 6,
-                direction: "none",
-                random: false,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: { enable: false, rotateX: 600, rotateY: 1200 },
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
               },
             },
-            interactivity: {
-              detect_on: "canvas",
-              events: {
-                onhover: { enable: true, mode: "bubble" },
-                onclick: { enable: true, mode: "repulse" },
-                resize: true,
-              },
-              modes: {
-                grab: { distance: 400, line_linked: { opacity: 1 } },
-                bubble: {
-                  distance: 107,
-                  size: 20,
-                  duration: 2,
-                  opacity: 0.3,
-                  speed: 3,
-                },
-                repulse: { distance: 79.9143318362715, duration: 0.4 },
-                push: { particles_nb: 4 },
-                remove: { particles_nb: 2 },
-              },
+            size: {
+              value: 5,
+              random: true,
+              anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
             },
-            retina_detect: true,
-          }}
-        />
-      </div>
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: "#ffffff",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: "none",
+              random: true,
+              straight: false,
+              bounce: true,
+              attract: { enable: false, rotateX: 600, rotateY: 1200 },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
       <div className="navbar">
-        <p>Julian Cesaro</p>
+        <img src={julian} alt="Julian" width={80}></img>
+        <p className="name">Julian Cesaro</p>
+        <div className="links">
+          <p>About Me</p>
+          <p>Projects</p>
+          <p>Resume</p>
+        </div>
+      </div>
+      <div className="content-card">
+        <div className="content">
+          <p className="content-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="content-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="content-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     </div>
   )
