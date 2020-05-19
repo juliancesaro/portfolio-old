1f9e61f2e2056bb3db7c1e7978b5d14e6543fc2d
juliancesaro <julianjcesaro@gmail.com>
2020-05-19 16:39:50 +1000

    added facebook logo

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,6 +3,7 @@ import "./Contact.css"
 import ContentCard from "../contentCard/ContentCard"
 import linkedin from "../../images/linkedin.png"
 import github from "../../images/github.png"
+import facebook from "../../images/facebook.png"
 
 const Contact = () => {
   return (
@@ -11,18 +12,26 @@ const Contact = () => {
         <p className="email">julianjcesaro@gmail.com</p>
         <div className="logos">
           <a
+            href="https://github.com/juliancesaro"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="Github Logo" width="50px" />
+          </a>
+          <a
             href="https://www.linkedin.com/in/julian-cesaro-015b9a156/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src={linkedin} alt="Linkedin Logo" width="40px" />
           </a>
+
           <a
-            href="https://github.com/juliancesaro"
+            href="https://www.facebook.com/julian.cesaro.1/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={github} alt="Github Logo" width="50x" />
+            <img src={facebook} alt="Facebook Logo" width="50px" />
           </a>
         </div>
       </div>
